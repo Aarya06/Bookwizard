4e40d464a33b480763205123bc3ba038dd9a06ea
root <raj.aarya06@gmail.com>
2020-05-18 12:52:34 +0000

    hide correct

--- public/javascript/flash.js
+++ public/javascript/flash.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
     setTimeout(function() {
-        $("#msg").hide()
+        $(".alert-danger").hide()
+    }, 2500);
+});
+
+$(document).ready(function(){
+    setTimeout(function() {
+        $(".alert-success").hide()
     }, 2500);
 });
